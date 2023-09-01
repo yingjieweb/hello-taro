d648b34ddbe703ef981531f540aaff4b465fb91e
yingjieweb <yingjieweb@qq.com>
2023-09-01 15:02:04 +0800

    feat: 🎸 Add tabbar config and icons

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,12 +1,34 @@
 export default defineAppConfig({
-  pages: [
-    'pages/index/index',
-    'pages/hooks/index',
-  ],
+  tabBar: {
+    list: [
+      {
+        pagePath: "pages/index/index",
+        text: "首页",
+        iconPath: "./assets/tabbar/home.png",
+        selectedIconPath: "./assets/tabbar/home-active.png",
+      },
+      {
+        pagePath: "pages/hooks/index",
+        text: "Hooks",
+        iconPath: "./assets/tabbar/hooks.png",
+        selectedIconPath: "./assets/tabbar/hooks-active.png",
+      },
+      {
+        pagePath: "pages/routes/index",
+        text: "Routes",
+        iconPath: "./assets/tabbar/routes.png",
+        selectedIconPath: "./assets/tabbar/routes-active.png",
+      },
+    ],
+    backgroundColor: "#3030b9",
+    selectedColor: "#fff",
+    // position: 'bottom', // 默认 bottom, 可选 top|bottom
+  },
+  pages: ["pages/index/index", "pages/hooks/index", "pages/routes/index"],
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-})
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black",
+  },
+});
